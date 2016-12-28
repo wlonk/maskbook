c398817843b631c1d1244708a7f55b059efe6c2c
Kit La Touche <kit@transneptune.net>
2016-12-28 00:48:30 -0700

    Clean up test -> spec transition

---
+++ spec/models/villain_spec.rb
@@ -0,0 +1,4 @@
+require "rails_helper"
+
+RSpec.describe Villain, :type => :model do
+end
--- spec/models/villain_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class VillainTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
