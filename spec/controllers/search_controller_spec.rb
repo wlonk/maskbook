897c11c2d926712ccfc4f86f08c507d1c7a84f28
Kit La Touche <kit@transneptune.net>
2017-01-10 01:12:27 -0700

    Bring coverage up to 100%
    With maximum kludge.
    
    Fixes #29.

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe SearchController, type: :controller do
       get :search
       expect(response).to have_http_status(:success)
     end
+
+    it "handles actual query strings" do
+      get :search, params: { s: "gen:gold gen:orange tag:blue user:kit free search" }
+      expect(response).to have_http_status(:success)
+    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
+
+    it "also allows you to filter by tag on a user" do
+      user = create(:user)
+      get :show, params: { id: user.id, tags: "orange" }
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
   end
 end
